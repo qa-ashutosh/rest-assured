737693d05d690ab62944d641e8083bf1db311082
Ashutosh <ashutosh@techaheadcorp.com>
2021-08-27 16:18:59 +0530

    Removed the selenium code for the OAuthTest

--- src/test/java/OAuth/OAuthTest.java
+++ src/test/java/OAuth/OAuthTest.java
@@ -1,24 +1,11 @@
 package OAuth;
 
 import io.restassured.path.json.JsonPath;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static io.restassured.RestAssured.*;
 
 public class OAuthTest {
     public static void main(String[] args) {
-
-       /* System.setProperty("webdriver.chrome.driver","/Users/ashu/Tools/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://accounts.google.com/o/oauth2/v2/auth/identifier?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&auth_url=https%3A%2F%2Faccounts.google.com%2Fo%2Foauth2%2Fv2%2Fauth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https%3A%2F%2Frahulshettyacademy.com%2FgetCourse.php&flowName=GeneralOAuthFlow");
-        driver.findElement(By.cssSelector("input[type='email']")).sendKeys("ashutosh");
-        driver.findElement(By.cssSelector("input[type='email']")).sendKeys(Keys.ENTER);
-        driver.findElement(By.cssSelector("input[type='password']")).sendKeys("ashutosh");
-        driver.findElement(By.cssSelector("input[type='password']")).sendKeys(Keys.ENTER);
-        String url = driver.getCurrentUrl();*/
 
         String url = "https://rahulshettyacademy.com/getCourse.php?code=4%2F0AX4XfWi2d1ciZieM04l27bDWpNADK6nL6g3tjqk8HQfiC7J2tXVltnT9803S6Uro9qj0LA&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=consent#";
         String partialCode = url.split("code=")[1];
