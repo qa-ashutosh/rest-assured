2ca1306c9e5a2e0acb2d442688c7ab154d554232
Ashutosh <ashutosh@techaheadcorp.com>
2021-08-28 15:37:32 +0530

    Added the POJO classes

--- src/test/java/OAuth/OAuthTest.java
+++ src/test/java/OAuth/OAuthTest.java
@@ -1,13 +1,23 @@
 package OAuth;
 
+import Pojo.Api;
+import Pojo.GetCourse;
+import Pojo.WebAutomation;
+import io.restassured.parsing.Parser;
 import io.restassured.path.json.JsonPath;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
 public class OAuthTest {
     public static void main(String[] args) {
 
-        String url = "https://rahulshettyacademy.com/getCourse.php?code=4%2F0AX4XfWi2d1ciZieM04l27bDWpNADK6nL6g3tjqk8HQfiC7J2tXVltnT9803S6Uro9qj0LA&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=consent#";
+        // Url for google login
+        // (for manual login process: then after login paste the new url in below variable 'String url')
+        // https://accounts.google.com/o/oauth2/v2/auth/identifier?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&auth_url=https%3A%2F%2Faccounts.google.com%2Fo%2Foauth2%2Fv2%2Fauth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https%3A%2F%2Frahulshettyacademy.com%2FgetCourse.php&flowName=GeneralOAuthFlow
+
+        String url = "https://rahulshettyacademy.com/getCourse.php?code=4%2F0AX4XfWhW4zw4hNRYqZ_RxlDnN7Vimen4jl2dTv-PEq95zomsu-2JmLKbqBLAVyhdL0Z4OA&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=none#";
         String partialCode = url.split("code=")[1];
         String code = partialCode.split("&scope")[0];
         System.out.println(code);
@@ -26,12 +36,30 @@ public class OAuthTest {
         String accessToken = js.getString("access_token");
 
 
-        String response = given().queryParam("access_token", accessToken)
-                .when().log().all()
-                .get("https://rahulshettyacademy.com/getCourse.php").asString();
+        // Now we are using POJO class here 'GetCourse'
+        GetCourse getCourse = given().queryParam("access_token", accessToken)
+                .expect().defaultParser(Parser.JSON)  // to tell the script that what type of response format we are giving to Pojo class
+                .when()
+                .get("https://rahulshettyacademy.com/getCourse.php").as(GetCourse.class);
 
-        System.out.println(response);
+        System.out.println(getCourse.getLinkedIn());
+        System.out.println(getCourse.getInstructor());
 
+        System.out.println(getCourse.getCourses().getApi().get(1).getCourseTitle());
+        System.out.println(getCourse.getCourses().getApi().get(1).getPrice());
+
+        // we dynamically getting the price of SoapUI testing course's price below
+        List<Api> apiCourses = getCourse.getCourses().getApi();
+        for(int i=0; i< apiCourses.size(); i++){
+            if(apiCourses.get(i).getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing"))
+                System.out.println(apiCourses.get(i).getPrice());
+        }
+
+        // Fetching all course titles of VebAutomation courses
+        List<WebAutomation> webAutomationCourses = getCourse.getCourses().getWebAutomation();
+        for(int i=0; i< webAutomationCourses.size(); i++){
+            System.out.println(webAutomationCourses.get(i).getCourseTitle());
+        }
     }
 
 }
